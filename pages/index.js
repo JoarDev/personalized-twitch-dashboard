b83dec7fb70eca0e4170e4d1a94f2629bc42d2b5
Joaquin Ramos <43919047+masterdrop@users.noreply.github.com>
2021-08-22 00:24:53 +0000

    added data persistence with replit key-value database

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import StreamerGrid from "../components/StreamerGrid"
 
 const Home = () => {
@@ -31,6 +31,8 @@ const Home = () => {
 
       console.log("From the server", json.data)
       setchannelsList(prevState => [...prevState, json.data])
+
+      await setChannel(value)
       e.target.elements.name.value = ""
     }
   }
@@ -46,16 +48,102 @@ const Home = () => {
     </div>
   )
 
+  const setChannel = async channelName => {
+    try {
+      const currentStreamers = channelsList.map(channel => channel.display_name.toLowerCase())
+
+      const streamerList = [...currentStreamers, channelName].join(",")
+
+      const path = `https://${window.location.hostname}`
+
+      const response = await fetch(`${path}/api/database`, {
+        method: `POST`,
+        body: JSON.stringify({
+          key: `CHANNELS`,
+          value: streamerList
+        })
+      })
+
+      if (response.status === 200) {
+        console.log(`Set ${channelName} in DB.`)
+      }
+
+
+    } catch (error) {
+      console.warn(error.message)
+    }
+  }
+
+  useEffect(() => {
+    console.log("fetching channels...")
+    fetchChannels()
+  }, [])
+
+  const fetchChannels = async () => {
+    try {
+      const path = `https://${window.location.hostname}`
+
+      const response = await fetch(`${path}/api/database`, {
+        method: "POST",
+        body: JSON.stringify({
+          action: "GET_CHANNELS",
+          key: "CHANNELS"
+        })
+      })
+
+      if (response.status === 404) {
+        console.log("DB response error (key not found)")
+      }
+
+      const json = await response.json()
+
+      
+
+      if (json.data) {
+        const channelNames = json.data.split(",")
+
+        console.log("CHANNEL NAMES: ", channelNames)
+
+        const channelData = []
+
+        for await (const channelName of channelNames) {
+          console.log("Getting Twitch Data for: ", channelName)
+
+          const channelResp = await fetch(`${path}/api/twitch`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ data: channelName })
+          })
+
+          const json = await channelResp.json()
+
+          if (json.data) {
+            //setchannelsList(prev => [...prev,json.data])
+            channelData.push(json.data)
+            console.log(channelData)
+          }
+
+          
+        }
+        setchannelsList(channelData)
+      }
+    } catch (error) {
+      console.warn(error.message)
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Head>
         <title>🎥 Personal Twitch Dashboard</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className={styles.inputContainer}>        
+      <div className={styles.inputContainer}>
         <div className={styles.inputContainer}>
           {renderForm()}
-          <StreamerGrid channels={channelsList} setChannel={setchannelsList}/>
+          <StreamerGrid channels={channelsList} setChannel={setchannelsList} />
         </div>
       </div>
     </div>
